3fb02cce1844a7f823ee4fcf10293e6456ff5459
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-07-16 18:36:47 +0000

    bug fixed! printing two digits combination from 00 to 90 except if the second number is less than or equal to the first

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(90):
-    if (i % 10 == 0):
+    if (i % 10 <= i // 10):
         continue
     else:
         print("{:02d}".format(i), end="\n" if i == 89 else ", ")
